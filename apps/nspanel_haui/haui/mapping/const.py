e03f0858e33bbf33e63f4852bc49d683c156950b
Shaun Chong <shaun@ninjavan.co>
2025-11-26 00:32:21 +0800

    Refactor MQTT controller to use AppDaemon instance name for topic prefix and remove static topic prefix configuration in constants, enhancing flexibility and clarity in MQTT communication.

--- apps/nspanel_haui/haui/mapping/const.py
+++ apps/nspanel_haui/haui/mapping/const.py
@@ -152,9 +152,7 @@ DEFAULT_CONFIG = {
         "sound_on_notification": True,
     },
     # mqtt related settings
-    "mqtt": {
-        "topic_prefix": "nspanel_haui/nspanel_haui",
-    },
+    "mqtt": {},
     # connection related settings
     "connection": {
         "heartbeat_interval": None,  # Default 5 sec, None means use interval provided by device, value in seconds
